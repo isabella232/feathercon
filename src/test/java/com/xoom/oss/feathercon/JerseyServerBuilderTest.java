c8ce558a848626578a449cdcf2a4e2031a07ba1f
Mark Petrovic <mark.petrovic@xoom.com>
2013-09-09 13:49:29 -0700

    break out unit tests for Jersey and static content server

--- src/test/java/com/xoom/oss/feathercon/JerseyServerBuilderTest.java
+++ src/test/java/com/xoom/oss/feathercon/JerseyServerBuilderTest.java
@@ -1,16 +1,43 @@
 package com.xoom.oss.feathercon;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.api.json.JSONConfiguration;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.core.MediaType;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class JerseyServerBuilderTest {
+
+    private FeatherCon server;
+
     @Before
     public void setUp() throws Exception {
-
+        JerseyServerBuilder jerseyServerBuilder = new JerseyServerBuilder("com.xoom.oss.feathercon", "/api/*");
+        server = jerseyServerBuilder.build();
+        server.start();
     }
 
     @After
     public void tearDown() throws Exception {
+        server.stop();
+    }
 
+    @Test
+    public void testJerseyBuilder() throws Exception {
+        ClientConfig clientConfig = new DefaultClientConfig();
+        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+        Client client = Client.create(clientConfig);
+        WebResource resource = client.resource("http://localhost:8080/api/users");
+        User user = resource.accept(MediaType.APPLICATION_JSON_TYPE).get(User.class);
+        assertThat(user.name, equalTo("Bob Loblaw"));
+        assertThat(user.emailAddress, equalTo("bob@lawbomb.example.com"));
     }
 }
--- src/test/java/com/xoom/oss/feathercon/WebappServerBuilderTest.java
+++ src/test/java/com/xoom/oss/feathercon/WebappServerBuilderTest.java
@@ -1,22 +1,50 @@
 package com.xoom.oss.feathercon;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.api.json.JSONConfiguration;
 import com.sun.jersey.spi.container.servlet.ServletContainer;
 import org.eclipse.jetty.servlet.DefaultServlet;
+import org.hamcrest.core.IsEqual;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class WebappServerBuilderTest {
 
+    private String restPathSpec = "/api/*";
+    private int serverPort = 8888;
+    private String jerseyScanPackages = "com.xoom.oss.feathercon";
+    private WebappServerBuilder webappServerBuilder;
+    private String resourceBase;
+    private FeatherCon server;
+
+    @Before
+    public void setup() throws Exception {
+        String resourcePath = getClass().getResource("/content-root/anchor").getFile();
+        resourceBase = new File(resourcePath).getParentFile().getAbsolutePath() + "/static/";
+        webappServerBuilder = new WebappServerBuilder(jerseyScanPackages, restPathSpec, resourceBase, serverPort);
+        server = webappServerBuilder.build();
+        server.start();
+    }
+
+    @After
+    public void teardown() throws Exception {
+        server.stop();
+    }
+
     @Test
     public void testWebAppBuilder() throws Exception {
-        String resourceBase = "/tmp";
-        String restPathSpec = "/api/*";
-        int serverPort = 8888;
-        String jerseyScanPackages = "com.xoom.oss.feathercon";
-        WebappServerBuilder webappServerBuilder = new WebappServerBuilder(jerseyScanPackages, restPathSpec, resourceBase, serverPort);
-
         assertThat(webappServerBuilder.servletConfigurations.size(), equalTo(2));
 
         ServletConfiguration jaxRsConfig = webappServerBuilder.servletConfigurations.get(0);
@@ -29,11 +57,28 @@ public class WebappServerBuilderTest {
         assertThat(staticConfig.servletClass.equals(DefaultServlet.class), equalTo(true));
         assertThat(staticConfig.initParameters.get("resourceBase"), equalTo(resourceBase));
         assertThat(staticConfig.pathSpecs.get(0), equalTo("/"));
-
-        FeatherCon server = webappServerBuilder.build();
         assertThat(server.port, equalTo(serverPort));
         assertThat(server.contextName, equalTo("/"));
-
         server.toString();
+    }
+
+    @Test
+    public void testStaticContentServer() throws Exception {
+        ClientConfig clientConfig = new DefaultClientConfig();
+        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+        Client client = Client.create(clientConfig);
+        WebResource resource = client.resource(String.format("http://localhost:%d/hello.html", serverPort));
+        ClientResponse clientResponse = resource.get(ClientResponse.class);
+        String clientHtml = clientResponse.getEntity(String.class);
+
+        FileReader fileReader = new FileReader(String.format("%s/hello.html", resourceBase));
+        BufferedReader br = new BufferedReader(fileReader);
+        String s;
+        StringBuilder persistentHtml = new StringBuilder();
+        while ((s = br.readLine()) != null) {
+            persistentHtml.append(s).append('\n');
+        }
+
+        assertThat(clientHtml, IsEqual.equalTo(persistentHtml.toString()));
     }
 }
