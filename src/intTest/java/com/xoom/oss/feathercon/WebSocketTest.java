737e87ffdec06ba616a38e37eaa2e4af214d8a77
Mark S. Petrovic <mspetrovic@gmail.com>
2013-12-04 20:20:12 -0800

    intTest

--- src/intTest/java/com/xoom/oss/feathercon/WebSocketTest.java
+++ src/intTest/java/com/xoom/oss/feathercon/WebSocketTest.java
@@ -3,12 +3,16 @@ package com.xoom.oss.feathercon;
 import org.junit.Test;
 
 import javax.websocket.CloseReason;
+import javax.websocket.EncodeException;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class WebSocketTest {
     @Test
@@ -22,21 +26,31 @@ public class WebSocketTest {
     }
 
     @ServerEndpoint(value = "/events/")
-    private class MyEndpoint {
+    public static class MyEndpoint {
+        Set<Session> sessions = new HashSet<Session>();
+
         @OnOpen
         public void onWebSocketConnect(Session sess) {
+            sessions.add(sess);
+            System.out.println("@@@ A");
         }
 
         @OnMessage
-        public void onWebSocketText(String message) {
+        public void onWebSocketText(String message) throws IOException, EncodeException {
+            System.out.println("@@@ B");
+            for (final Session session : sessions) {
+                session.getBasicRemote().sendObject(message);
+            }
         }
 
         @OnClose
         public void onWebSocketClose(CloseReason reason) {
+            System.out.println("@@@ C");
         }
 
         @OnError
         public void onWebSocketError(Throwable cause) {
+            System.out.println("@@@ D");
         }
     }
 }
